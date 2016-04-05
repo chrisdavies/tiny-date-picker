a7bbb3cc0008c80a6b4073291961645f5c18c70d
Chris Davies <ferociousturtle@gmail.com>
2016-04-05 12:32:48 -0400

    Fire change event on the input when user selects a date

--- tiny-date-picker.js
+++ tiny-date-picker.js
@@ -9,9 +9,27 @@ function TinyDatePicker(input, options) {
   var currentDate = opts.parse(input.value);
   var el = buildCalendarElement(currentDate, opts);
   var isHiding = false; // Used to prevent the calendar from showing when transitioning to hidden
-  var focusCatcher = htmlToElement('<button style="position:absolute;border:0;background:transparent;"></button>');
+  var focusCatcher = htmlToElement('<button style="position: absolute; width: 1; height: 1; overflow: hidden; border: 0; background: transparent;"></button>');
   var body = document.body;
+  var CustomEvent = window.CustomEvent;
   input.readOnly = true;
+
+
+  /////////////////////////////////////////////////////////
+  // Unintrusive polyfill the custom event for IE9+
+  (function () {
+    if (typeof CustomEvent === 'function') return false;
+
+    CustomEvent = function (event, params) {
+      params = params || { bubbles: false, cancelable: false, detail: undefined };
+      var evt = document.createEvent('CustomEvent');
+      evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+      return evt;
+    }
+
+    CustomEvent.prototype = window.Event.prototype;
+  })();
+
 
   /////////////////////////////////////////////////////////
   // Event handling/state management
@@ -97,6 +115,9 @@ function TinyDatePicker(input, options) {
     input.value = date ? opts.format(date) : '';
     setDate(date);
     hide();
+
+    // Make sure the input fires its change event
+    input.dispatchEvent(new CustomEvent('change', { bubbles: true }));
   }
 
   function setDate(date) {
@@ -283,7 +304,6 @@ function TinyDatePicker(input, options) {
     return div.firstChild;
   }
 }
-
 
 /////////////////////////////////////////////////////////
 // Commonjs support
