1e7c0871bd25a73d36bc2e24aacb99721da6b371
Chris Davies <ferociousturtle@gmail.com>
2017-09-13 06:54:38 -0400

    v3 Fix dp-below positioning

--- src/mode/dropdown-mode.js
+++ src/mode/dropdown-mode.js
@@ -34,23 +34,25 @@ function autoPosition(input, dp) {
 
 function adjustCalX(dp, inputPos, docEl) {
   var cal = dp.el;
-  var viewWidth = docEl.clientWidth;
-  var calWidth = cal.offsetWidth;
-  var calRight = inputPos.left + calWidth;
-  var shouldLeftAlign = calRight < viewWidth || inputPos.right < calWidth;
-  var left = inputPos.left - (shouldLeftAlign ? 0 : calRight - viewWidth) + document.body.scrollLeft;
+  var scrollLeft = docEl.scrollLeft;
+  var inputLeft = inputPos.left + scrollLeft;
+  var maxRight = docEl.clientWidth + scrollLeft;
+  var offsetWidth = cal.offsetWidth;
+  var calRight = inputLeft + offsetWidth;
+  var shiftedLeft = maxRight - offsetWidth;;
+  var left = calRight > maxRight && shiftedLeft > 0 ? shiftedLeft : inputLeft;
 
   cal.style.left = left + 'px';
 }
 
 function adjustCalY(dp, inputPos, docEl) {
   var cal = dp.el;
-  var viewHeight = docEl.clientHeight;
+  var scrollTop = docEl.scrollTop;
+  var inputTop = scrollTop + inputPos.top;
   var calHeight = cal.offsetHeight;
-  var scrollTop = document.body.scrollTop;
-  var calBottom = inputPos.bottom + 8 + calHeight + scrollTop;
-  var isAbove = calBottom > viewHeight && inputPos.top > calHeight;
-  var top = scrollTop + inputPos.top + (isAbove ? -calHeight - 8 : inputPos.height + 8);
+  var belowTop = inputTop + inputPos.height + 8;
+  var aboveTop = inputTop - calHeight - 8;
+  var top = (aboveTop > 0 && belowTop + calHeight > scrollTop + docEl.clientHeight) ? aboveTop : belowTop;
 
   cal.style.top = top + 'px';
 }
